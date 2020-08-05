74bceea145d26731cb317636602e5a879cc9e6f3
Nauman Munir <naumanmunir@gmail.com>
2020-08-05 23:25:59 +0500

    bakery shop - Vanilla JS - DOM

---
+++ js/app.js
@@ -0,0 +1,47 @@
+// filter buttons
+(function () {
+  // select all buttons
+  const filterBtn = document.querySelectorAll(".filter-btn");
+
+  filterBtn.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const value = e.target.dataset.filter;
+      const items = document.querySelectorAll(".store-item");
+      items.forEach(function (item) {
+        if (value === "all") {
+          item.style.display = "block";
+        } else {
+          if (item.classList.contains(value)) {
+            item.style.display = "block";
+          } else {
+            item.style.display = "none";
+          }
+        }
+      });
+    });
+  });
+})();
+
+// Search Input
+(function () {
+  // target search box
+  const search = document.getElementById("search-item");
+  search.addEventListener("keyup", function () {
+    let value = search.value.toLowerCase().trim();
+
+    const items = document.querySelectorAll(".store-item");
+    items.forEach(function (item) {
+      let type = item.dataset.item;
+
+      let length = value.length;
+      let match = type.slice(0, length);
+      if (value === match) {
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
+    });
+  });
+})();
